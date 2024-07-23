3ee15085135042a090ff9857c46659de31bc4fbc
Iosef Pineda <iosefpineda@outlook.com>
2024-07-23 10:04:16 -0600

    Se hacen ajustes para despliegue a producción

--- src/ui/hooks/useAdminAuth.js
+++ src/ui/hooks/useAdminAuth.js
@@ -11,8 +11,8 @@ export const useAdminAuth = () => {
         try {
             const { data } = await elMerkadeoAPI.post('/auth-admin/login-admin', { email, password });
 
-            // document.cookie = `auth-token=${ data.authToken }; max-age=1200; domain=.elmerkadeo.com; path=/; samesite=none; secure`;
-            document.cookie = `auth-token=${ data.authToken }; max-age=1200`;
+            document.cookie = `auth-token=${ data.authToken }; max-age=1200; domain=.elmerkadeo.com; path=/; samesite=none; secure`;
+            // document.cookie = `auth-token=${ data.authToken }; max-age=1200`;
             window.location.href = import.meta.env.VITE_ADMIN_SITE_URL;
             return true;
         } catch (error) {
